7269859d174e2079b4dac252940a69f39be49ae4
pankaj batham <codebypankaj@gmail.com>
2022-10-18 13:16:02 +0530

    about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import About from './../components/About/About';
+
+export default function about() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <About />
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
       <div className="container">
         <Hero />
         <About />
-        <CallOut />
       </div>
     </>
   );
