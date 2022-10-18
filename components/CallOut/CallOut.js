48391475d6bab70b9e61915ef00ce83d6e8660ef
pankaj batham <codebypankaj@gmail.com>
2022-10-18 13:03:23 +0530

    Update code

--- components/CallOut/CallOut.js
+++ components/CallOut/CallOut.js
@@ -1,21 +1,21 @@
 import { useEffect } from 'react';
 import classes from './CallOut.module.css';
-import sliderImage from './../../../public/images/img_524387.png';
+import sliderImage from './../../public/images/img_524387.png';
 
 const CallOut = () => {
   useEffect(() => {
-    var handle = $('#custom-handle');
-    $('#slider').slider({
-      value: 50,
-      slide: function (event, ui) {
-        if (ui.value < 4) {
-          window.open(
-            'https://api.whatsapp.com/send/?phone=8963957654&text=Hi+there&app_absent=0',
-            '_blank'
-          );
-        }
-      },
-    });
+    // var handle = $('#custom-handle');
+    // $('#slider').slider({
+    //   value: 50,
+    //   slide: function (event, ui) {
+    //     if (ui.value < 4) {
+    //       window.open(
+    //         'https://api.whatsapp.com/send/?phone=8963957654&text=Hi+there&app_absent=0',
+    //         '_blank'
+    //       );
+    //     }
+    //   },
+    // });
   }, []);
 
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import About from './components/About/About';
-import CallOut from './components/CallOut/CallOut';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
+import About from './../components/About/About';
+import CallOut from './../components/CallOut/CallOut';
+import Header from './../components/Header/Header';
+import Hero from './../components/Hero/Hero';
 
 export default function Home() {
   return (
