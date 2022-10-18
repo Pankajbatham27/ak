6c760ed2790bec71ad729c1140247b41a4b5e02d
pankaj batham <codebypankaj@gmail.com>
2022-10-18 15:04:12 +0530

    new content

--- components/About/About.js
+++ components/About/About.js
@@ -10,13 +10,13 @@ const About = () => {
         </div>
         <div className="col-md-8">
           <p>
-            Aliquam erat volutpat. Duis ac turpis. Donec sit amet
-            eros. Lorem ipsum dolor sit amet, consectetuer adipiscing
-            elit. Mauris fermentum dictum magna. Sed laoreet aliquam
-            leo. Ut tellus dolor, dapibus eget, elementum vel, cursus
-            eleifend, elit. Aenean auctor wisi et urna. Aliquam erat
-            volutpat. Duis ac turpis. Integer rutrum ante eu
-            lacus.Vestibulum libero nisl, porta vel.{' '}
+            Pankaj Aliquam erat volutpat. Duis ac turpis. Donec sit
+            amet eros. Lorem ipsum dolor sit amet, consectetuer
+            adipiscing elit. Mauris fermentum dictum magna. Sed
+            laoreet aliquam leo. Ut tellus dolor, dapibus eget,
+            elementum vel, cursus eleifend, elit. Aenean auctor wisi
+            et urna. Aliquam erat volutpat. Duis ac turpis. Integer
+            rutrum ante eu lacus.Vestibulum libero nisl, porta vel.{' '}
           </p>
 
           <p>
