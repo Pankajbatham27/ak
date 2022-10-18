6b4b8ddce1374e049e638e711e6fb302216d18c5
pankaj batham <codebypankaj@gmail.com>
2022-10-18 12:51:59 +0530

    Update Script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -25,21 +26,21 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600&display=swap"
           rel="stylesheet"
         ></link>
-
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossorigin="anonymous"
-        ></script>
-
-        <link
-          rel="stylesheet"
-          href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css"
-        />
-
-        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
       </Head>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+      ></Script>
+
+      <link
+        rel="stylesheet"
+        href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css"
+      />
+
+      <Script src="https://code.jquery.com/jquery-3.6.0.js"></Script>
+      <Script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></Script>
+
       <Component {...pageProps} />
     </>
   );
