a1a87171e0b5c94397cdcd27bf5ca3d74e741537
pankaj batham <codebypankaj@gmail.com>
2022-11-03 16:12:14 +0530

    mobile nav

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -18,21 +18,7 @@ const Header = () => {
       <div className={classes.navCenter}>
         <nav className="navbar navbar-expand-lg navbar-light">
           <div className="container-fluid">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarNavAltMarkup"
-            >
+            <div className="" id="navbarNavAltMarkup">
               <div className="navbar-nav">
                 <Link href="/">
                   <a className="nav-link active" aria-current="page">
