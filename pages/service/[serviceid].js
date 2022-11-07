96e76587d8185e09edcfb47c0bbfdadf367dceb1
pankaj batham <codebypankaj@gmail.com>
2022-11-07 16:28:53 +0530

    new

--- pages/service/[serviceid].js
+++ pages/service/[serviceid].js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import Gallery from '../../components/Service/Gallery';
 import Header from './../../components/Header/Header';
 
 const ServiceDetails = () => {
+  const [serviceImage, setServiceImage] = useState([]);
+
+  async function getdata() {
+    const response = await fetch(
+      'http://localhost/tune/service/wedding-photography'
+    );
+    const res = await response.json();
+    setServiceImage(res);
+  }
+  useEffect(() => {
+    getdata();
+  }, []);
+
   return (
     <>
       <Header />
-      <Gallery />
+      <Gallery serviceImage={serviceImage} />
     </>
   );
 };
