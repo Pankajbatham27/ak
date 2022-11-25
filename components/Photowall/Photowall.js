8cfbf7533cec540828856c808c6cf1ad589f26de
pankaj batham <codebypankaj@gmail.com>
2022-11-25 13:04:58 +0530

    photowall loading issue solved

--- components/Photowall/Photowall.js
+++ components/Photowall/Photowall.js
@@ -22,73 +22,65 @@ const Photowall = () => {
   const componentRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
-      // console.log(componentRef.current.clientWidth);
+    var totalLi = $('#ulWidth li').length;
 
-      var totalLi = $('#ulWidth li').length;
+    if (componentRef.current.clientWidth > 1295) {
+      $('#ulWidth').css('column-count', 12);
+      var liWidth = componentRef.current.clientWidth / 12;
 
-      if (componentRef.current.clientWidth > 1295) {
-        $('#ulWidth').css('column-count', 12);
-        var liWidth = componentRef.current.clientWidth / 12;
-
-        for (let index = totalLi; index > 12 * 3; index--) {
-          $('#ulWidth li:nth-child(' + index + ')').remove();
-        }
-      } else if (componentRef.current.clientWidth > 1115) {
-        $('#ulWidth').css('column-count', 9);
-        var liWidth = componentRef.current.clientWidth / 9;
-
-        for (let index = totalLi; index > 9 * 3; index--) {
-          $('#ulWidth li:nth-child(' + index + ')').remove();
-        }
-      } else if (componentRef.current.clientWidth > 935) {
-        $('#ulWidth').css('column-count', 6);
-        var liWidth = componentRef.current.clientWidth / 6;
-
-        for (let index = totalLi; index > 6 * 3; index--) {
-          $('#ulWidth li:nth-child(' + index + ')').remove();
-        }
-      } else if (componentRef.current.clientWidth > 695) {
-        $('#ulWidth').css('column-count', 6);
-        var liWidth = componentRef.current.clientWidth / 6;
-
-        for (let index = totalLi; index > 6 * 3; index--) {
-          $('#ulWidth li:nth-child(' + index + ')').remove();
-        }
-      } else if (componentRef.current.clientWidth > 515) {
-        $('#ulWidth').css('column-count', 6);
-        var liWidth = componentRef.current.clientWidth / 6;
-
-        for (let index = totalLi; index > 6 * 3; index--) {
-          $('#ulWidth li:nth-child(' + index + ')').remove();
-        }
-      } else if (componentRef.current.clientWidth < 515) {
-        $('#ulWidth').css('column-count', 4);
-        var liWidth = componentRef.current.clientWidth / 4;
-
-        for (let index = totalLi; index > 4 * 3; index--) {
-          $('#ulWidth li:nth-child(' + index + ')').remove();
-        }
+      for (let index = totalLi; index > 12 * 3; index--) {
+        $('#ulWidth li:nth-child(' + index + ')').remove();
       }
+    } else if (componentRef.current.clientWidth > 1115) {
+      $('#ulWidth').css('column-count', 9);
+      var liWidth = componentRef.current.clientWidth / 9;
 
-      $('#ulWidth li').css('height', liWidth);
-      $('#ulWidth li').css('width', liWidth);
-    }, 1000);
-  }, []);
+      for (let index = totalLi; index > 9 * 3; index--) {
+        $('#ulWidth li:nth-child(' + index + ')').remove();
+      }
+    } else if (componentRef.current.clientWidth > 935) {
+      $('#ulWidth').css('column-count', 6);
+      var liWidth = componentRef.current.clientWidth / 6;
+
+      for (let index = totalLi; index > 6 * 3; index--) {
+        $('#ulWidth li:nth-child(' + index + ')').remove();
+      }
+    } else if (componentRef.current.clientWidth > 695) {
+      $('#ulWidth').css('column-count', 6);
+      var liWidth = componentRef.current.clientWidth / 6;
+
+      for (let index = totalLi; index > 6 * 3; index--) {
+        $('#ulWidth li:nth-child(' + index + ')').remove();
+      }
+    } else if (componentRef.current.clientWidth > 515) {
+      $('#ulWidth').css('column-count', 6);
+      var liWidth = componentRef.current.clientWidth / 6;
+
+      for (let index = totalLi; index > 6 * 3; index--) {
+        $('#ulWidth li:nth-child(' + index + ')').remove();
+      }
+    } else if (componentRef.current.clientWidth < 515) {
+      $('#ulWidth').css('column-count', 4);
+      var liWidth = componentRef.current.clientWidth / 4;
+
+      for (let index = totalLi; index > 4 * 3; index--) {
+        $('#ulWidth li:nth-child(' + index + ')').remove();
+      }
+    }
+
+    $('#ulWidth li').css('height', liWidth);
+    $('#ulWidth li').css('width', liWidth);
+  }, [allPhotos]);
 
   useEffect(() => {
     setInterval(() => {
       var min = 1;
       var max = $('#ulWidth li').length;
-
       var randNumber = Math.floor(Math.random() * (max - min)) + min;
-
       var randNumber2 = Math.floor(Math.random() * (max - min)) + min;
-
       $('#ulWidth li:nth-child(' + randNumber + ') img')
         .fadeOut(1000)
         .fadeIn(1000);
-
       $('#ulWidth li:nth-child(' + randNumber2 + ') img')
         .fadeOut(1400)
         .fadeIn(1400);
